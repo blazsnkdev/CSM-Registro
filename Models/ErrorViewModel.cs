2050d114f2615e0343fccc9c8052b163ff7bb920
blaz snk <blasasto0914@gmail.com>
2025-06-19 11:42:44 -0500

    primer commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CSM_Registro.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
